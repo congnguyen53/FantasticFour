9ab2dab5c8f433d1879f6c1795b995df6eea0a1e
James Haskell <jhaskell@hartford.edu>
2020-04-03 16:54:49 -0400

    Login button function

--- PythonFile/Login.py
+++ PythonFile/Login.py
@@ -6,45 +6,56 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-
+from PySide2.QtWidgets import QApplication, QPushButton
 from PyQt5 import QtCore, QtGui, QtWidgets
+from OrderTest import Ui_MainWindow1
 
+class Ui_Login(object):
+    #Define this function
+    def openWindow(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_MainWindow1() #Use class name of GUI you want to open
+        self.ui.setupUi(self.window)
+        self.window.show()
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(759, 518)
-        self.loginButton = QtWidgets.QPushButton(Dialog)
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(759, 518)
+        self.loginButton = QtWidgets.QPushButton(MainWindow)
         self.loginButton.setGeometry(QtCore.QRect(240, 340, 93, 28))
         self.loginButton.setObjectName("loginButton")
-        self.registerButton = QtWidgets.QPushButton(Dialog)
+
+        #Click event - loginButton is object name, openWindow is function name
+        self.loginButton.clicked.connect(self.openWindow)
+
+        self.registerButton = QtWidgets.QPushButton(MainWindow)
         self.registerButton.setGeometry(QtCore.QRect(420, 340, 93, 28))
         self.registerButton.setObjectName("registerButton")
-        self.MD_title = QtWidgets.QLabel(Dialog)
+        self.MD_title = QtWidgets.QLabel(MainWindow)
         self.MD_title.setGeometry(QtCore.QRect(260, 80, 221, 71))
         font = QtGui.QFont()
         font.setFamily("Sitka Subheading")
         font.setPointSize(20)
         self.MD_title.setFont(font)
         self.MD_title.setObjectName("MD_title")
-        self.MD_title2 = QtWidgets.QLabel(Dialog)
+        self.MD_title2 = QtWidgets.QLabel(MainWindow)
         self.MD_title2.setGeometry(QtCore.QRect(140, 150, 481, 71))
         font = QtGui.QFont()
         font.setFamily("Sitka Subheading")
         font.setPointSize(20)
         self.MD_title2.setFont(font)
         self.MD_title2.setObjectName("MD_title2")
-        self.loginFormPassIn = QtWidgets.QLineEdit(Dialog)
+        self.loginFormPassIn = QtWidgets.QLineEdit(MainWindow)
         self.loginFormPassIn.setGeometry(QtCore.QRect(334, 300, 182, 22))
         self.loginFormPassIn.setObjectName("loginFormPassIn")
-        self.loginFormPassLabel = QtWidgets.QLabel(Dialog)
+        self.loginFormPassLabel = QtWidgets.QLabel(MainWindow)
         self.loginFormPassLabel.setGeometry(QtCore.QRect(241, 300, 83, 22))
         self.loginFormPassLabel.setObjectName("loginFormPassLabel")
-        self.loginFormUserIn = QtWidgets.QLineEdit(Dialog)
+        self.loginFormUserIn = QtWidgets.QLineEdit(MainWindow)
         self.loginFormUserIn.setGeometry(QtCore.QRect(334, 271, 182, 22))
         self.loginFormUserIn.setText("")
         self.loginFormUserIn.setObjectName("loginFormUserIn")
-        self.loginFormUserLabel = QtWidgets.QLabel(Dialog)
+        self.loginFormUserLabel = QtWidgets.QLabel(MainWindow)
         self.loginFormUserLabel.setGeometry(QtCore.QRect(241, 271, 86, 22))
         self.loginFormUserLabel.setObjectName("loginFormUserLabel")
         self.loginButton.raise_()
@@ -56,12 +67,12 @@ class Ui_Dialog(object):
         self.loginFormUserIn.raise_()
         self.loginFormUserLabel.raise_()
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        MainWindow.setWindowTitle(_translate("Dialog", "Dialog"))
         self.loginButton.setText(_translate("Dialog", "Login"))
         self.registerButton.setText(_translate("Dialog", "Register"))
         self.MD_title.setText(_translate("Dialog", "Medical Doctor"))
@@ -69,12 +80,11 @@ class Ui_Dialog(object):
         self.loginFormPassLabel.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt; font-weight:600;\">Password</span></p></body></html>"))
         self.loginFormUserLabel.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt; font-weight:600;\">Username</span></p></body></html>"))
 
-
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_Login()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
