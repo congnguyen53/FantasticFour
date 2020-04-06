e5a64c8e4d12c07b6d0ba482818156031243794e
James Haskell <60665168+jhaskell16@users.noreply.github.com>
2020-04-06 18:37:51 -0400

    Update main.py

--- PythonFile/main.py
+++ PythonFile/main.py
@@ -5,34 +5,47 @@ from New_patient_registration import *
 
 
 class Dialog(QtWidgets.QDialog, Ui_Dialog):
+    def openMain(self):
+        self.mainScreen = MainScreen()
+        self.mainScreen.show()
+        self.hide()
+
     def __init__(self, parent=None):
         super(Dialog, self).__init__(parent)
         self.setupUi(self)
-        #self.mainScreen = MainScreen()
-        #self.loginButton.clicked.connect(self.mainScreen.show)
-        self.loginButton.clicked.connect(self.close)
+
+        self.loginButton.clicked.connect(self.openMain)
 
 
 class MainScreen(QtWidgets.QMainWindow, Ui_MainScreen):
+    def openLogin(self):
+        self.NPR = Dialog()
+        self.NPR.show()
+        self.hide()
+
+    def openNPR(self):
+        self.NPR = NPRegistration()
+        self.NPR.show()
+
     def __init__(self, parent=None):
         super(MainScreen, self).__init__(parent)
         self.setupUi(self)
-        self.login = Dialog()
-        self.Main_logout.clicked.connect(self.login.show)
-        self.Main_logout.clicked.connect(self.close)
-        self.NPR = NPRegistration()
-        self.Main_registerPatient.clicked.connect(self.NPR.show)
+        self.Main_logout.clicked.connect(self.openLogin)
+        self.Main_registerPatient.clicked.connect(self.openNPR)
 
 class NPRegistration(QtWidgets.QMainWindow, Ui_NP_Registration):
+    def closeSelf(self):
+        self.hide()
+
     def __init__(self, parent=None):
         super(NPRegistration, self).__init__(parent)
         self.setupUi(self)
-        self.NP_cancel.clicked.connect(self.hide)
+        self.NP_cancel.clicked.connect(self.closeSelf)
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    w = MainScreen()
+    w = Dialog()
     w.show()
     sys.exit(app.exec_())
